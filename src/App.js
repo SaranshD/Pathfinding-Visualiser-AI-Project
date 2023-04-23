a4f57b0e770ac96405d54bc408106b5f714b60d6
saran <saran@SARANSHD>
2023-04-23 16:10:44 +0530

    Add project files.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PathfindingVisualiser from './Pathfinding Visualiser/PathfindingVisualiser.jsx';  
+
+import './Pathfinding Visualiser/PathfindingVisualiser.css'
+
+export default function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualiser></PathfindingVisualiser>
+    </div>
+  );
+}
